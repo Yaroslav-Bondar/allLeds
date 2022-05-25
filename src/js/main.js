254fd51f4127640345f9fe66df7c0e930b7af62e
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-05-25 19:27:59 +0300

    header adaptive before 768px brkp

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const sum = require('./module/sum.js');
 const multiple = require('./module/multiple.js');
 require('./module/burger/burger.js');
+require('./module/dynamicAdapt/dynamicAdapt.js');
+
 console.log(sum(4, 5));
 console.log(multiple(5, 5));
 
