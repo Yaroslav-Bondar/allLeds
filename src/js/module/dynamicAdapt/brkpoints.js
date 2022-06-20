360f5ad03f1497e3a88a2c2c08432ec3287da92e
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-06-20 10:29:03 +0300

    phone header menu prototype

--- src/js/module/dynamicAdapt/brkpoints.js
+++ src/js/module/dynamicAdapt/brkpoints.js
@@ -1,34 +1,48 @@
 
-// const constRoot = require('../constants/root.js');
-// const BREAK_POINT_MD = constRoot.BREAK_POINT_MD;
-// const menuList = constRoot.menuList;
-
-
-
-// dynamic adaptive for mobile menu
-// if(window.innerWidth <= BREAK_POINT_MD) {
-//     // adding a container for dynamic adaptive
-//     if(!menuList.querySelector('.menu__item_da')) {
-//         const menuListDa = document.createElement('li');
-//         menuListDa.classList.add('menu__item', 'menu__item_da');
-//         menuListDa.textContent = 'i am adaptive';
-//         menuList.prepend(menuListDa);
-//     }
-// }
-// window.addEventListener('resize', () => {
-//     if(window.innerWidth <= BREAK_POINT_MD) {
-//         // adding a container for dynamic adaptive
-//         if(!menuList.querySelector('.menu__item_da')) {
-//             // const menuListDa = document.createElement('li');
-//             // menuListDa.classList.add('menu__item', 'menu__item_da');
-//             // menuListDa.textContent = 'i am adaptive';
-//             // menuList.prepend(menuListDa);
-//         }
-//     } else {
-//         // remove a container for dynamic adaptive
-//         const menuItemDa = menuList.querySelector('.menu__item_da'); 
-//         if(menuItemDa) {
-//             menuItemDa.remove();
-//         }
-//     }
-// });
+const constRoot = require('../constants/root.js');
+const BREAK_POINT_MD = constRoot.BREAK_POINT_MD;
+const contactsHeaderWhatsapp = document.querySelector('.contacts-header__whatsapp');
+const contactsHeaderWhatsappPrevText = contactsHeaderWhatsapp.textContent;
+const contactsHeaderColumnPhone = document.querySelector('.contacts-header__column_phone');
+// console.log(contactsHeaderColumnPhone);
+// change the internal text for WhatsApp
+if(window.innerWidth <= BREAK_POINT_MD) {
+    contactsHeaderWhatsapp.textContent = 'WhatsApp';
+}
+window.addEventListener('resize', () => {
+    if(window.innerWidth <= BREAK_POINT_MD) {
+        // change text for WhatsApp
+        contactsHeaderWhatsapp.textContent = 'WhatsApp';
+        // adding container to change phone
+        if(!contactsHeaderColumnPhone.querySelector('.contacts-header__phone-container')) {
+            const contactsHeaderPhoneContainer = document.createElement('div');
+            const contactsHeaderPhoneMenu = document.createElement('div');
+            contactsHeaderPhoneMenu.classList.add('contacts-header__phone-menu');
+            contactsHeaderPhoneContainer.classList.add('contacts-header__phone-container');
+            const contactsAmount = contactsHeaderColumnPhone.children.length;
+            // move childrens from contactsHeaderColumnPhone to contactsHeaderPhoneContainer and contactsHeaderPhoneMenu
+            for(let i = 0; i < contactsAmount; i++) {
+                if(i !== 0) {
+                    contactsHeaderPhoneContainer.append(contactsHeaderColumnPhone.children[0]);
+                } else {
+                    contactsHeaderPhoneMenu.append(contactsHeaderColumnPhone.children[0]);
+                } 
+            }
+            // insert phone menu
+            contactsHeaderColumnPhone.append(contactsHeaderPhoneContainer);
+            contactsHeaderColumnPhone.append(contactsHeaderPhoneMenu);
+            contactsHeaderColumnPhone.addEventListener('click', e => {
+                const target = e.target;
+                // show/close menu
+                contactsHeaderColumnPhone.classList.toggle('contacts-header__column_active');
+                // move the selected phone to the container
+                if(target.classList.contains('contacts-header__phone') && target.closest('.contacts-header__phone-container')) {
+                    contactsHeaderPhoneMenu.append(target);
+                    contactsHeaderPhoneContainer.append(contactsHeaderPhoneMenu.querySelector('.contacts-header__phone'));
+                }
+            });     
+        }
+    } else {
+        contactsHeaderWhatsapp.textContent = contactsHeaderWhatsappPrevText;
+    }
+});
