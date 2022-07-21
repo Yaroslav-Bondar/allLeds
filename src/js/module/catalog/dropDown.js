e689aeb0695669a6028733119b260d7f62f5fc10
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-07-21 20:35:46 +0300

    the main logic of the catalog side menu

--- src/js/module/catalog/dropDown.js
+++ src/js/module/catalog/dropDown.js
@@ -2,31 +2,33 @@
 const constRoot = require('../constants/root');
 
 const catalogMenu = constRoot.catalogMenu;
+// previous and current menu item
+let prev = current = null;
 
 catalogMenu.addEventListener('click', e => {
     const catalogItem = e.target.closest('.menu-catalog__item');
-    // const catalogContent = catalogItem.querySelector('.menu-catalog__content');
-    // const catalogSubmenu = catalogItem.querySelector('.menu-catalog__submenu');
-    // close/open drop-down menu
-    // if(catalogContent) {
-        
-        // close/open dropdown menu
-        catalogItem.classList.toggle('menu-catalog__item_active');
-        // close all open child dropdown menus
-        if(!catalogItem.classList.contains('menu-catalog__item_active')) {
-            const activeItems = catalogItem.querySelectorAll('.menu-catalog__item_active'); 
-            if(activeItems.length) {
-                activeItems.forEach(item => {
-                    item.classList.remove('menu-catalog__item_active');
-                }); 
-            }
+    console.log(catalogItem);
+    // close/open dropdown menu
+    catalogItem.classList.toggle('menu-catalog__item_active');
 
+    if(catalogItem.classList.contains('menu-catalog__item_active')) {
+        // get current menu item
+        current = catalogItem.querySelector('.menu-catalog__submenu') || catalogItem.querySelector('.menu-catalog__link');
+        // remove the highlight of the previous active menu item if 
+        // there was a previous one and if the previous one is not equal to the current one
+        if(prev && prev !== current) prev.removeAttribute('data-current');
+        // highlight current menu item
+        current.setAttribute('data-current', 'true');
+        prev = current;
+    } else {
+        // remove the highlight of the previous active menu item
+        prev.removeAttribute('data-current');
+        const activeItems = catalogItem.querySelectorAll('.menu-catalog__item_active'); 
+        if(activeItems.length) {
+            activeItems.forEach(item => {
+                // close all open child dropdown menus
+                item.classList.remove('menu-catalog__item_active');
+            }); 
         }
-        // catalogContent.classList.toggle('menu-catalog__content_active');
-        // catalogSubmenu.classList.toggle('menu-catalog__submenu_arrow-down');
-        // some things
-    // }
-    // if(catalogSubmenu)
-
-
+    }
 });
